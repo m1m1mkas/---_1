b980eb568fb66d66a242a0050ea27229156a6e35
m1m1m <m1m1mkas@mail.ru>
2022-08-23 22:11:25 +0300

    task_3

---
+++ task_3/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число:");
+ int a = Convert.ToInt32(Console.ReadLine());
+
+if (a % 2 == 1)
+{
+Console.WriteLine("Число " + a + " является: НЕЧЁТНЫМ");
+}
+else
+{
+Console.WriteLine("Число " + a + "является: ЧЁТНЫМ");
+}
