196617de43ba0d39bd43c34009c24eef8bd6a7be
m1m1m <m1m1mkas@mail.ru>
2022-08-23 22:11:52 +0300

    Task_4

---
+++ task_4/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите число:");
+int a = Convert.ToInt32(Console.ReadLine());
+int i = 1;
+
+Console.WriteLine("Чётные числа от 1 до " + a);
+while (i <= a)
+{
+    if (i % 2 != 1)
+    {
+        Console.Write(i + ", ");
+    }
+    i++;
+}
+
